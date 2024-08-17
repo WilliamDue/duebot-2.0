969698e631c8e51f23e6fb13d2e09a68bafda6e5
Due <williamhenrichdue@gmail.com>
2024-08-17 10:25:51 +0200

    Using Haskell now.

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for DueBot
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
--- shell.nix
+++ shell.nix
@@ -1,9 +1,18 @@
 let
-  pkgs = import <nixpkgs> {};
-in pkgs.mkShell {
-  packages = [
-    (pkgs.python3.withPackages (python-pkgs: [
-      python-pkgs.discordpy
-    ]))
-  ];
-}
+  pkgs = import <nixpkgs> { config.allowUnfree = true; };
+  unstable = import <nixos-unstable> { };
+  compilerVersion = "ghc96"; 
+  compiler = pkgs.haskell.packages."${compilerVersion}";
+in
+let pkg =
+      compiler.developPackage {
+        root = ./.;
+        modifier = drv:
+          pkgs.haskell.lib.addBuildTools drv
+            (with pkgs.haskellPackages;
+              [ cabal-install
+                ghcid
+                haskell-language-server
+              ]);
+      };
+in pkg
