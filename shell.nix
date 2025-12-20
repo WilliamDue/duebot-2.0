b5d205ba361f1910caa8025f5d2bafcd5750a212
Due <williamhenrichdue@gmail.com>
2025-12-21 00:43:50 +0100

    Gonna make my bad code better later.

--- shell.nix
+++ shell.nix
@@ -2,14 +2,24 @@ let
   pkgs = import <nixpkgs> { config.allowUnfree = true; };
   unstable = import <nixos-unstable> { };
   compilerVersion = "ghc98"; 
-  compiler = pkgs.haskell.packages."${compilerVersion}";
+  compiler = pkgs. haskell.packages."${compilerVersion}";
 in
 let pkg =
       compiler.developPackage {
-        root = ./.;
+        root = ./. ;
+        overrides = self:  super: {
+          discord-haskell = pkgs.haskell.lib. overrideCabal super.discord-haskell (old: {
+            version = "1.18.0";
+            src = pkgs.fetchurl {
+              url = "https://hackage.haskell.org/package/discord-haskell-1.18.0/discord-haskell-1.18.0.tar.gz";
+              sha256 = "sha256-DB9OstockuzDWVCXmvYExemGQQX4P1dMMZtq7CSkfTw=";
+            };
+            doCheck = false;
+          });
+        };
         modifier = drv:
           pkgs.haskell.lib.addBuildTools drv
-            (with pkgs.haskellPackages;
+            (with compiler;
               [ cabal-install
                 ghcid
                 haskell-language-server
--- src/DueBot/Main.hs
+++ src/DueBot/Main.hs
@@ -1,19 +1,21 @@
 module DueBot.Main (main) where
 
-import Relude
 import Data.FileEmbed
 import Discord
 import DueBot.EventHandler
+import Relude
 
 token :: Text
 token = $(embedStringFile ".token")
 
 main :: IO ()
 main = do
-    userFacingError <- runDiscord $ def
-             { discordToken = token
-             , discordOnEvent = eventHandler
-             , discordOnLog = \s -> putTextLn s >> putTextLn ""
-             }
+  userFacingError <-
+    runDiscord
+      $ def
+        { discordToken = token,
+          discordOnEvent = eventHandler,
+          discordOnLog = \s -> putTextLn s >> putTextLn ""
+        }
 
-    putTextLn userFacingError
+  putTextLn userFacingError
